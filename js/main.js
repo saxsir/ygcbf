28cd6822ec23a31638d5f70577f578f14fe4a7f0
saxsir <shohei.sasamoto@gmail.com>
2014-07-14 15:03:29 +0900

    add debug log

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ window.fbAsyncInit = function () {
         alert('データ取得エラー');
       }
       else {
-        console.log(result);
+        console.log(JSON.stringify(result));
         ygcbf.analyze(result);
       }
     });
